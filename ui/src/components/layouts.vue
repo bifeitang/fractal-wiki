0d22a43eaf8eb397e4d6fc265b8c562ee112ad2f
Yang Hu <yh2948@columbia.edu>
2018-07-19 11:38:46 -0600

    without node modules

---
+++ ui/src/components/layouts.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="container">
+    <section class="banner">
+    </section>
+
+    <section class="tree">
+      <!-- write the list here -->
+    </section>
+
+    <section class="card_list">
+      <button class="card_button" v-on:dblclick="displayCard">
+        <card v-if="show_editor">
+        </card>
+        <button>submit</button>
+      </button>
+    </section>
+
+  </section>
+</template>
+
+<script>
+  import card from './card';
+
+  export default {
+    data() {
+      return {
+        show_editor: false
+      };
+    },
+    components: {
+      card
+    },
+    methods: {
+      displayCard(){
+        this.show_editor = !this.show_editor;
+      }
+    }
+  };
+</script>
+
+<style>
+@import '../common/style/layout.css';
+
+</style>
---
+++ ui/src/components/nestedCards.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- loop over the division -->
+  <div id="nested_cardlist">
+    
+  </div>
+</template>
+
+<script>
+  // Local Registration
+  import markdownEditor from 'vue-simplemde/src/markdown-editor'
+
+  export default {
+    name:'nested_cardlist',
+    componets: {
+      markdownEditor
+    }
+  }
+</script>
+
+<style>
+
+</style>

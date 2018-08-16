d3d6d91a0b13bd794c2e502b263860e3486af7c0
Lisa Jetton <ldjetton21@gmail.com>
2018-08-16 00:26:29 -0600

    upates ui

--- vue/src/components/Layout.vue
+++ vue/src/components/Layout.vue
@@ -1,21 +1,16 @@
 <template lang="html">
   <el-container>
-  <el-header>Fractal Wiki</el-header>
-  <el-container>
-    <el-aside width="300px">
-      <ContentDisplay>
-      </ContentDisplay>
+  <el-header class="page-header">Fractal Wiki</el-header>
+  <el-container class="overview-container">
+    <el-aside class="aside" max-width="300px">
+      <ContentDisplay></ContentDisplay>
       <!-- <BasicCard></BasicCard> -->
     </el-aside>
-
-    <el-container>
-      <el-main>
-        <ContentDisplay></ContentDisplay>
-        <!-- <BasicCard></BasicCard> -->
-      </el-main>
+    <el-container class="main">
+      <ContentDisplay></ContentDisplay>
+      <!-- <BasicCard></BasicCard> -->
     </el-container>
-
-  </el-container>
+  </el-container >
 </el-container>
 
 </template>
@@ -32,4 +27,43 @@ export default {
 </script>
 
 <style lang="css">
+  .page-header {
+    height: 75px !important;
+    font-family: 'Goudy Bookletter 1911', serif;
+    font-weight: bold;
+    font-size: 3em;
+    border: 5px double #44444d;
+  }
+
+  .overview-container {
+    display: flex;
+  }
+
+  .aside {
+    width: 100%;
+    height: 100%;
+    margin: 10px;
+    background: #838f9e;
+    font-size: 20px;
+    color: #c5c5d1;
+    text-align: left;
+    border: 2px solid #5d837d;
+    flex-shrink: none;
+    flex: 0 1 auto !important;
+  }
+
+  .main {
+    width: 100%;
+    height: 100%;
+    margin: 10px;
+    background: #838f9e;
+    font-size: 20px;
+    color: #c5c5d1;
+    text-align: left;
+    border: 2px solid #B3C0D1;
+    padding: 6px !important;
+    flex-shrink: none;
+    flex: 0 1 auto !important;
+  }
+
 </style>
